bedaab99c03124dc76e824b94fa8e87979d339a3
HCHu <hchu@live.com>
2022-11-22 13:21:14 +0800

    add typeorm to individual module

--- src/individual/individual.module.ts
+++ src/individual/individual.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { IndividualCustomerService } from './individual.service';
+import { Entities } from './individual.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature(Entities)],
   providers: [IndividualCustomerService],
   exports: [IndividualCustomerService],
 })
